52ae82061ad89d775cae43f67d54369bc4c00961
Sean <sglover42@gmail.com>
2023-08-15 21:19:34 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './brioux-falls-logo.PNG';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello Human. We aren't ready for you just yet.
+        </p>
+        <div></div>
+      </header>
+    </div>
+  );
+}
+
+export default App;

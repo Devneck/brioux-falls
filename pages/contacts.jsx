9a382fbc6b7dd832a27cb213c974d8ce737997e0
Sean <sglover42@gmail.com>
2024-04-18 20:08:46 -0500

    add directions to contacts page

--- pages/contacts.jsx
+++ pages/contacts.jsx
@@ -104,6 +104,76 @@ const Contacts = () => {
           />
         </div>
       </section>
+      <section className="section kf-contacts-form">
+        <div className="container" style={{ padding: '3rem 0 0 0' }}>
+          <div className="kf-titles">
+            <div
+              className="kf-subtitle element-anim-1 scroll-animate"
+              data-animate="active"
+            >
+              How to find us
+            </div>
+            <img src="images/steel-district-map.png" alt="map of the steel district" style={{ width: '100%', margin: '2rem 0' }}/>
+            <h3
+              className="kf-title element-anim-1 scroll-animate"
+              data-animate="active"
+            >
+              Steel District Visitor Parking Instructions
+            </h3>
+          </div>
+          <div
+            className="kf-text element-anim-1 scroll-animate"
+            data-animate="active"
+          >
+            <p>
+              Enter The Steel District off E. 6th Street, take a right to access the entrance
+              of the parking ramp. Stay to the right while entering the parking ramp and proceed
+              up the ramp and to the right.
+            </p>
+            <h5>
+              Please only park on levels 2, 3 & 4
+            </h5>
+            <p>
+              Skywalk access is located on Level 3 of the parking ramp. Use the Skywalk access to enter
+              the Office tower.
+            </p>
+          </div>
+          <hr />
+          <div className="kf-titles">
+            <h3
+              className="kf-title element-anim-1 scroll-animate"
+              data-animate="active"
+            >
+              Walking Instructions
+            </h3>
+          </div>
+          <div
+            className="kf-text element-anim-1 scroll-animate"
+            data-animate="active"
+          >
+            <h5>
+              Garage Entrance
+            </h5>
+            <p>
+              Enter The Steel District off E 4th Place north of the parking ramp (Identified as
+              "Deliveries Only" in the map, construction may be in progress in the area). Enter
+              through the first door after the bricks/retail strip on the right to gain access
+              to the parking garage. Take stairs or elevator to 3rd floor of parking garage.
+              Go out into the parking garage and use the Skywalk access to enter the Office tower.
+            </p>
+            <h5>
+              Office Tower Entrance
+            </h5>
+            <p>
+              Enter The Steel District off E 4th Place just north of the parking ramp (Identified as
+              "Deliveries Only" in the map, construction may be in progress in the area). Enter
+              the office tower main entrance on the left to gain access to the Office tower.
+              Take stairs or elevator to 2nd floor of the Office tower. Take a left and follow
+              the hall all the way to the end on the North side, take a left to find Brioux Falls.
+            </p>
+          </div>
+        </div>
+      </section>
       {/* Section Contacts Form */}
       {/* <section className="section kf-contacts-form">
         <div className="container">
